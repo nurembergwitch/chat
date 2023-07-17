cc61176e0977d0e59c8f3333f9922e26fb01da22
Mads <ishiodareiji@gmail.com>
2023-07-18 01:03:02 +0300

    final helm chart edits

--- k8s/chat-v2/deploy-nginx-ingress.sh
+++ k8s/chat-v2/deploy-nginx-ingress.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 deploychart(){
-    APP_VERSION="1.8.0"
-    CHART_VERSION="4.7.0"   
+    APP_VERSION="1.8.1"
+    CHART_VERSION="4.7.1"   
     #mkdir nginx-ingress-controller-manifests && cd nginx-ingress-controller-manifests
     helm template ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --version ${CHART_VERSION} --namespace ingress-nginx > ./01-nginx-ingress.${APP_VERSION}.yaml
 }
--- k8s/chat-v2/helm/templates/_common-blocks.tpl
+++ k8s/chat-v2/helm/templates/_common-blocks.tpl
@@ -1,4 +1,4 @@
 {{- define "metadata" -}}
-name: {{ .Values.webapp.name | default "chat "}} # used to have -service/ingress suffix
-namespace: {{ .Values.webapp.name | default "chat" }}
+name: {{ .Values.webapp.name | default "chat "}}
+namespace: {{ .Values.webapp.namespace | default "chat" }}
 {{- end -}}
